71782e00002fa92e422f406cc1f49f01d03754ea
alex mccooeye <flamincodedev@gmail.com>
2018-09-28 15:14:35 -0400

    basics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
 import './App.css';
-import { VoicePlayer, VoiceRecognition } from 'react-voice-components'
+import { VoicePlayer, VoiceRecognition } from 'react-voice-components';
+// var pdfText = require('pdf-text');
+// var pathToPdf = '/file' + "/Stanley.pdf";
 
 class App extends Component {
   constructor (props) {
@@ -12,6 +14,7 @@ class App extends Component {
     }
     this.startRecording = this.startRecording.bind(this)
     this.stopRecording = this.stopRecording.bind(this)
+    this.registerRecording = this.registerRecording.bind(this)
   }
   
   startRecording () {
@@ -21,14 +24,30 @@ class App extends Component {
   stopRecording () {
     this.setState({recording: false})
   }
+
+  registerRecording (result) {
+    let transcript = result.finalTranscript
+    console.error(transcript)
+    this.setState({recordedResult: transcript})
+  }
+
+  getText () {
+    // pdfText(pathToPdf, function(err, chunks) {
+    //   if (err) console.error(err)
+    //   console.error(chunks)
+    // })
+  }
   
   render() {
     return (
       <div className="App">
         <VoicePlayer
           play
-          text="Testing the voice player"
+          text="Testing"
         />
+        <Button
+          onClick={this.getText}
+        >Get Text</Button>
         <Button
           onClick={this.startRecording}
         >Start Recognition</Button>
@@ -38,11 +57,14 @@ class App extends Component {
         {this.state.recording ?
           <VoiceRecognition
             continuous
-            onResult={(e) => {alert(e)}}
-            onStart={(e) => {alert(e)}}
-            onEnd={(e) => {alert(e)}}
-            onError={(e) => {alert(e)}}
+            onResult={(e) => {this.registerRecording(e)}}
+            // onStart={(e) => {console.error(e)}}
+            // onEnd={(e) => {console.error(e)}}
+            // onError={(e) => {console.error(e)}}
           /> : ''}
+        {this.state.recordedResult ? 
+          <div>{this.state.recordedResult}</div>
+          : ''}
       </div>
     );
   }
